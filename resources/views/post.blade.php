1bec281959532cdaa454932d9afb0f2b7f3f6473
Edwin Cardenas <eacardenase@unal.edu.co>
2024-01-30 19:02:19 -0500

    feat: add posts and single post view

---
+++ resources/views/post.blade.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>My Post</title>
+    <link rel="stylesheet" href="app.css">
+</head>
+<body>
+    <article>
+        <h1>
+            <a href="/post">My First post</a>
+        </h1>
+        <p>
+            Lorem
+        </p>
+    </article>
+    <a href="/">Back</a>
+</body>
+</html>
---
+++ resources/views/posts.blade.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>My Blog</title>
+    <link rel="stylesheet" href="app.css">
+</head>
+<body>
+    <article>
+        <h1>
+            <a href="/post">My First post</a>
+        </h1>
+        <p>
+            Lorem
+        </p>
+    </article>
+    <article>
+        <h1>
+            <a href="/post">My Second post</a>
+        </h1>
+        <p>
+            Lorem
+        </p>
+    </article>
+    <article>
+        <h1>
+            <a href="/post">My Third post</a>
+        </h1>
+        <p>
+            Lorem
+        </p>
+    </article>
+</body>
+</html>
