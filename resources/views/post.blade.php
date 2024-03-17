d0c4794585f2a3c802a3291c59e1d3ee54926741
Edwin Cardenas <eacardenase@gmail.com>
2024-03-17 18:34:07 -0500

    refactor: use blade layouts

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,15 +1,6 @@
-<!doctype html>
-<html lang="en">
+@extends('layout')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>My Post</title>
-    <link rel="stylesheet" href="/app.css">
-</head>
-
-<body>
+@section('content')
     <article>
         <h1>{{ $post->title }}</h1>
         <div>
@@ -17,6 +8,4 @@
         </div>
     </article>
     <a href="/">Go Back</a>
-</body>
-
-</html>
+@endsection
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,23 +1,18 @@
-<!doctype html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>My Blog</title>
-        <link rel="stylesheet" href="app.css">
-    </head>
+@extends('layout')
 
-    <body>
-        @foreach($posts as $post)
-            <article>
-                <h1>
-                    <a href="/posts/{{ $post->slug }}">
-                        {{ $post->title }}
-                    </a>
-                </h1>
-                <div>{{ $post->excerpt }}</div>
-            </article>
-        @endforeach
-    </body>
-</html>
+@section('banner')
+    <h1>Latest's Blogs</h1>
+@endsection
+
+@section('content')
+    @foreach($posts as $post)
+        <article>
+            <h1>
+                <a href="/posts/{{ $post->slug }}">
+                    {{ $post->title }}
+                </a>
+            </h1>
+            <div>{{ $post->excerpt }}</div>
+        </article>
+    @endforeach
+@endsection
