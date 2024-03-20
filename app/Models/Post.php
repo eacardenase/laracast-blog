4ab60cf0a67b7abda65c245c255e0ca0db252007
Edwin Cardenas <eacardenase@gmail.com>
2024-03-20 11:23:54 -0500

    feat: mass asignment protection

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+    /*
+     * everything is fillable except for the following properties
+     * if empty means that we want mass assignment protection disabled
+     */
+    //    protected $guarded = [];
+
+    protected $fillable = ['title', 'excerpt', 'body']; // only accept the following properties
 }
