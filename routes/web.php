8a1bc6b1aea5665b92cd92293f2afa1f4176323e
Edwin Cardenas <eacardenase@gmail.com>
2024-03-21 19:25:53 -0500

    feat: add filter by category and hasMany relationship

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,8 @@
 <?php
 
+use App\Models\Category;
 use App\Models\Post;
 use Illuminate\Support\Facades\Route;
-
 
 /*
 |--------------------------------------------------------------------------
@@ -24,5 +24,11 @@ Route::get('/', function () {
 Route::get('posts/{post:slug}', function (Post $post) {
     return view('post', [
         'post' => $post,
+    ]);
+});
+
+Route::get('categories/{category:slug}', function (Category $category) {
+    return view('posts', [
+        'posts' => $category->posts,
     ]);
 });
