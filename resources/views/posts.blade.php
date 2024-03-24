6b41389f61a095811d642669742036a9c288c34b
Edwin Cardenas <eacardenase@gmail.com>
2024-03-23 20:37:53 -0500

    feat: makes featured post card dynamic

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -15,9 +15,9 @@
     {{--    @endforeach--}}
 
     @include('_posts-header')
-    
+
     <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
-        <x-post-featured-card/>
+        <x-post-featured-card :post="$posts->first()"/>
 
         <div class="lg:grid lg:grid-cols-2">
             <x-post-card/>
