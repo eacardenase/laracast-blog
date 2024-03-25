dbfbe1e3ff6cc48bed38c8fdbf2fe0b8e084816b
Edwin Cardenas <eacardenase@gmail.com>
2024-03-25 12:01:32 -0500

    feat: setup post page dynamically and extract category button component

--- resources/views/components/posts-grid.blade.php
+++ resources/views/components/posts-grid.blade.php
@@ -3,7 +3,7 @@
 <x-post-featured-card :post="$posts->first()"/>
 
 @if ($posts->count() > 1)
-    <div class="lg:grid lg:grid-cols-6">
+    <div class="lg:grid lg:gap-2 lg:grid-cols-6">
         @foreach ($posts->skip(1) as $post)
             <x-post-card
                 :post="$post"
