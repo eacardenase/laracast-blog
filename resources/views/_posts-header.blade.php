f14cb31d6b74c0d8680d31cbc9a3b31a764ab494
Edwin Cardenas <eacardenase@gmail.com>
2024-03-27 21:12:24 -0500

    refactor: extract chevron down component

--- resources/views/_posts-header.blade.php
+++ resources/views/_posts-header.blade.php
@@ -19,15 +19,7 @@
                     <button class="py-2 px-3 text-sm text-left font-semibold w-full lg:w-32 flex lg:inline-flex mr-3">
                         {{ isset($currentCategory) ? ucwords($currentCategory?->name) : "Categories" }}
 
-                        <svg class="transform -rotate-90 absolute pointer-events-none" style="right: 12px;" width="22"
-                             height="22" viewBox="0 0 22 22">
-                            <g fill="none" fill-rule="evenodd">
-                                <path stroke="#000" stroke-opacity=".012" stroke-width=".5" d="M21 1v20.16H.84V1z">
-                                </path>
-                                <path fill="#222"
-                                      d="M13.854 7.224l-3.847 3.856 3.847 3.856-1.184 1.184-5.04-5.04 5.04-5.04z"></path>
-                            </g>
-                        </svg>
+                        <x-down-arrow class="absolute pointer-events-none" style="right: 12px;"/>
                     </button>
                 </x-slot:trigger>
 
