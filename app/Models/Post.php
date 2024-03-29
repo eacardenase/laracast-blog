931e285f72238525b1d753e7810512d78c9677d8
Edwin Cardenas <eacardenase@gmail.com>
2024-03-29 14:12:10 -0500

    feat: use when method to only query when condition is met

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -32,10 +32,9 @@ class Post extends Model
 
     public function scopeFilter(Builder $query, array $filters)
     {
-        if ($filters['search'] ?? false) {
-            $query
-                ->where('title', 'like', '%'.request('search').'%')
-                ->orWhere('body', 'like', '%'.request('search').'%');
-        }
+        $query->when($filters['search'] ?? false, fn ($query, $search) => $query
+            ->where('title', 'like', "%$search%")
+            ->orWhere('body', 'like', "%$search%")
+        );
     }
 }
