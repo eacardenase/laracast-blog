3d773fc5464aae62f0d64fbe9cbf879e4fc6fdb5
Edwin Cardenas <eacardenase@gmail.com>
2024-03-30 14:45:00 -0500

    refactor: move posts related views into their own folder

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,21 +2,20 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Category;
 use App\Models\Post;
 
 class PostController extends Controller
 {
     public function index()
     {
-        return view('posts', [
+        return view('posts.index', [
             'posts' => Post::latest()->filter(request(['search', 'category']))->get(),
         ]);
     }
 
     public function show(Post $post)
     {
-        return view('post', [
+        return view('posts.show', [
             'post' => $post,
         ]);
     }
