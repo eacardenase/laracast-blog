00f5f7d5a5a0b65f432d3074445d44e12a742488
Edwin Cardenas <eacardenase@gmail.com>
2024-03-31 13:56:13 -0500

    bugfix: exclude page query param from url search

--- resources/views/components/category-dropdown.blade.php
+++ resources/views/components/category-dropdown.blade.php
@@ -7,14 +7,16 @@
         </button>
     </x-slot>
 
-    <x-dropdown-item href="/" :active="request()->routeIs('home')">All</x-dropdown-item>
-    
+    <x-dropdown-item
+        href="/?{{ http_build_query(request()->except('category', 'page')) }}"
+        :active="request()->routeIs('home')"
+    >
+        All
+    </x-dropdown-item>
+
     @foreach($categories as $category)
         <x-dropdown-item
-            {{--  href="/categories/{{ $category->slug }}"  --}}
-            href="?category={{ $category->slug }}&{{ http_build_query(request()->except('category')) }}"
-            {{--  :active="isset($currentCategory) && $currentCategory->is($category)"  --}}
-            {{--  :active="request()->is('*' . $category->slug)"  --}}
+            href="?category={{ $category->slug }}&{{ http_build_query(request()->except('category', 'page')) }}"
             :active="request()->is('categories/' . $category->slug)"
         >
             {{ ucwords($category->name) }}
