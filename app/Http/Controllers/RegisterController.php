5ff59aacd6cbdcda9958808a594a0d78c75cad49
Edwin Cardenas <eacardenase@gmail.com>
2024-03-31 21:59:22 -0500

    feat: add flash message on user registration

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -16,12 +16,15 @@ class RegisterController extends Controller
         $attributes = request()->validate([
             'name' => ['required', 'min:7', 'max:255'],
             'username' => ['required', 'min:3', 'max:255', 'unique:users'],
-            //  'username' => ['required', 'min:3', 'max:255', Rule::unique('users', 'username')], // alternative approach
             'email' => ['required', 'email', 'max:255', 'unique:users,email'], // alternative approach
             'password' => ['required', 'min:6', 'max:255'],
         ]);
 
         User::create($attributes);
+
+        // TODO: Sign in user
+
+        session()->flash('success', 'Your account has been created.');
 
         return redirect('/');
     }
--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -54,4 +54,11 @@
         </div>
     </footer>
 </section>
+@if(session()->has('success'))
+    <div class="fixed bottom-3 right-3 bg-blue-500 text-white py-2 px-4 rounded-xl text-sm">
+        <p>
+            {{ session('success') }}
+        </p>
+    </div>
+@endif
 </body>
