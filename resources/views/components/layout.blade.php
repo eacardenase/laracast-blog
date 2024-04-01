6aa70a5d50546cc129e21f5396cf3fe80ae95a98
Edwin Cardenas <eacardenase@gmail.com>
2024-03-31 22:03:21 -0500

    feat: hide flash message after timeout

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -55,7 +55,12 @@
     </footer>
 </section>
 @if(session()->has('success'))
-    <div class="fixed bottom-3 right-3 bg-blue-500 text-white py-2 px-4 rounded-xl text-sm">
+    <div
+        x-data="{ show: true}"
+        x-init="setTimeout(() => show = false, 2000)"
+        x-show="show"
+        class="fixed bottom-3 right-3 bg-blue-500 text-white py-2 px-4 rounded-xl text-sm"
+    >
         <p>
             {{ session('success') }}
         </p>
