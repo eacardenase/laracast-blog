13d9277514f178a5a21e96009a5db3dd30e2d3c4
Edwin Cardenas <eacardenase@gmail.com>
2024-03-31 22:07:00 -0500

    refactor: extract flash message into it's own component

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -55,15 +55,6 @@
     </footer>
 </section>
 @if(session()->has('success'))
-    <div
-        x-data="{ show: true}"
-        x-init="setTimeout(() => show = false, 2000)"
-        x-show="show"
-        class="fixed bottom-3 right-3 bg-blue-500 text-white py-2 px-4 rounded-xl text-sm"
-    >
-        <p>
-            {{ session('success') }}
-        </p>
-    </div>
+    <x-flash key="success"/>
 @endif
 </body>
