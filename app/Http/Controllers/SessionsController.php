09037275ae4ee789d669c7492d1f783ff0ffd2d3
Edwin Cardenas <eacardenase@gmail.com>
2024-04-02 18:21:24 -0500

    feat: add login functionality

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -6,6 +6,27 @@ use Illuminate\Http\Request;
 
 class SessionsController extends Controller
 {
+    public function create()
+    {
+        return view('sessions.create');
+    }
+
+    public function store()
+    {
+        $credentials = request()->validate([
+            'email' => ['required'],
+            'password' => ['required'],
+        ]);
+
+        if (auth()->attempt($credentials)) {
+            return redirect('/')->with('success', 'Welcome back!');
+        }
+
+        return back()
+            ->withInput()
+            ->withErrors(['email' => 'Your provided credentials could not be verified.']);
+    }
+
     public function destroy()
     {
         auth()->logout();
