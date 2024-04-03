c47104cebff3c40e348ad654bb3dccd359873e77
Edwin Cardenas <eacardenase@gmail.com>
2024-04-03 16:32:41 -0500

    feat: render comments dynamically

--- resources/views/components/post-comment.blade.php
+++ resources/views/components/post-comment.blade.php
@@ -1,3 +1,5 @@
+@props(['comment'])
+
 <article class="flex bg-gray-100 p-6 space-x-4 rounded-xl border border-gray-200">
     <div class="flex-shrink-0">
         <img class="rounded-full" src="https://i.pravatar.cc/60?img={{ rand(1, 70) }}" alt="avatar" height="60"
@@ -5,18 +7,15 @@
     </div>
     <div>
         <header class="mb-4">
-            <h3 class="font-bold">Jane Doe</h3>
+            <h3 class="font-bold">{{ $comment->author->username }}</h3>
             <p class="text-xs">
                 <time>
-                    Posted 8 months ago
+                    {{ $comment->created_at->diffForHumans() }}
                 </time>
             </p>
         </header>
         <p>
-            Reprehenderit duis occaecat id non eu dolor et. Nostrud tempor commodo amet laboris
-            culpa
-            dolore Lorem occaecat mollit. Velit ipsum magna nisi quis do cillum. Dolor sit pariatur
-            labore culpa officia id.
+            {{ $comment->body }}
         </p>
     </div>
 </article>
