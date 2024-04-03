f6eb7a58b7f47e4781b589334f76797ab9be0c53
Edwin Cardenas <eacardenase@gmail.com>
2024-04-03 07:44:28 -0500

    feat: add coment component markup

---
+++ resources/views/components/post-comment.blade.php
@@ -0,0 +1,22 @@
+<article class="flex bg-gray-100 p-6 space-x-4 rounded-xl border border-gray-200">
+    <div class="flex-shrink-0">
+        <img class="rounded-full" src="https://i.pravatar.cc/60?img={{ rand(1, 70) }}" alt="avatar" height="60"
+             width="60">
+    </div>
+    <div>
+        <header class="mb-4">
+            <h3 class="font-bold">Jane Doe</h3>
+            <p class="text-xs">
+                <time>
+                    Posted 8 months ago
+                </time>
+            </p>
+        </header>
+        <p>
+            Reprehenderit duis occaecat id non eu dolor et. Nostrud tempor commodo amet laboris
+            culpa
+            dolore Lorem occaecat mollit. Velit ipsum magna nisi quis do cillum. Dolor sit pariatur
+            labore culpa officia id.
+        </p>
+    </div>
+</article>
