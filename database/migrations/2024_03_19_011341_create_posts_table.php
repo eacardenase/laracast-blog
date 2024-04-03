e727b843fe1265dc68ee53ee5c1620ce89295706
Edwin Cardenas <eacardenase@gmail.com>
2024-04-03 15:24:15 -0500

    bugfix: rename categories migration in order to execute before posts migration

--- database/migrations/2024_03_19_011341_create_posts_table.php
+++ database/migrations/2024_03_19_011341_create_posts_table.php
@@ -4,7 +4,8 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
+return new class extends Migration
+{
     /**
      * Run the migrations.
      */
@@ -13,7 +14,7 @@ return new class extends Migration {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained()->cascadeOnDelete();
-            $table->foreignId('category_id');
+            $table->foreignId('category_id')->constrained()->cascadeOnDelete();
             $table->string('slug')->unique();
             $table->string('title');
             $table->text('excerpt');
