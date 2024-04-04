513433a5580037d6054e8d8dd593930ebc374766
Edwin Cardenas <eacardenase@gmail.com>
2024-04-04 17:38:17 -0500

    feat: setup mailchimp

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('ping', function () {
+    $mailchimp = new \MailchimpMarketing\ApiClient();
+
+    $mailchimp->setConfig([
+        'apiKey' => config('services.mailchimp.key'),
+        'server' => 'us22',
+    ]);
+
+    $response = $mailchimp->lists->addListMember('6a65f12e35', [
+        'email_address' => 'eacardenase@unal.edu.co',
+        'status' => 'subscribed',
+    ]);
+
+    ddd($response);
+});
+
 Route::get('/', [PostController::class, 'index'])->name('home');
 
 Route::get('posts/{post:slug}', [PostController::class, 'show']);
