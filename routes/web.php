66d634c8941983b60ce1aced24e3d76d317c8339
Edwin Cardenas <eacardenase@gmail.com>
2024-04-03 20:18:33 -0500

    feat: restrict comment posting if guest user on route level

--- routes/web.php
+++ routes/web.php
@@ -20,11 +20,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [PostController::class, 'index'])->name('home');
 
 Route::get('posts/{post:slug}', [PostController::class, 'show']);
-Route::post('posts/{post:slug}/comment', [PostCommentController::class, 'store']);
+Route::post('posts/{post:slug}/comment', [PostCommentController::class, 'store'])->middleware('auth');
 
 Route::get('register', [RegisterController::class, 'create'])->middleware('guest');
 Route::post('register', [RegisterController::class, 'store'])->middleware('guest');
 
-Route::get('/login', [SessionsController::class, 'create'])->middleware('guest');
+Route::get('/login', [SessionsController::class, 'create'])->middleware('guest')->name('login');
 Route::post('/login', [SessionsController::class, 'store']);
 Route::post('/logout', [SessionsController::class, 'destroy'])->middleware('auth');
