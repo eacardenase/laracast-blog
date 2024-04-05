f579a54e759a9aba2d1f1b9318684416b231b9a2
Edwin Cardenas <eacardenase@gmail.com>
2024-04-04 20:00:55 -0500

    refactor: store newsletter into service container

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,10 @@
 
 namespace App\Providers;
 
+use App\Services\Newsletter;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\ServiceProvider;
+use MailchimpMarketing\ApiClient;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -12,7 +14,14 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        app()->bind(Newsletter::class, function () {
+            $client = (new ApiClient())->setConfig([
+                'apiKey' => config('services.mailchimp.key'),
+                'server' => 'us22',
+            ]);
+
+            return new Newsletter($client);
+        });
     }
 
     /**
--- app/Services/Newsletter.php
+++ app/Services/Newsletter.php
@@ -7,23 +7,17 @@ use MailchimpMarketing\Configuration;
 
 class Newsletter
 {
+    public function __construct(protected ApiClient $client)
+    {
+    }
+
     public function subscribe(string $email, ?string $list = null)
     {
         $list ??= config('services.mailchimp.lists.subscribers');
 
-        return $this->client()->lists->addListMember($list, [
+        return $this->client->lists->addListMember($list, [
             'email_address' => $email,
             'status' => 'subscribed',
-        ]);
-    }
-
-    protected function client(): Configuration
-    {
-        $mailchimp = new ApiClient();
-
-        return $mailchimp->setConfig([
-            'apiKey' => config('services.mailchimp.key'),
-            'server' => 'us22',
         ]);
     }
 }
