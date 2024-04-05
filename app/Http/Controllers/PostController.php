dc179f7c4aa993d2e61ed5ea03e90bf1729f6f7b
Edwin Cardenas <eacardenase@gmail.com>
2024-04-05 14:14:03 -0500

    feat: add posts controller

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\Post;
 use Illuminate\Contracts\View\View;
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 use Symfony\Component\HttpFoundation\Response;
 
 class PostController extends Controller
@@ -27,5 +29,22 @@ class PostController extends Controller
     public function create(): View
     {
         return view('posts.create');
+    }
+
+    public function store(Request $request)
+    {
+        $attributes = $request->validate([
+            'title' => ['required'],
+            'slug' => ['required', Rule::unique('posts', 'slug')],
+            'excerpt' => ['required'],
+            'body' => ['required'],
+            'category_id' => ['required', Rule::exists('categories', 'id')],
+        ]);
+
+        $attributes['user_id'] = auth()->id();
+
+        Post::create($attributes);
+
+        return redirect('/');
     }
 }
