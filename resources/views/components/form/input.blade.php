258a2d96ea36e9221cef24b77b1c2003f072bdca
Edwin Cardenas <eacardenase@gmail.com>
2024-04-06 14:28:15 -0500

    feat: create settings panel and refactor login form

--- resources/views/components/form/input.blade.php
+++ resources/views/components/form/input.blade.php
@@ -1,16 +1,16 @@
-@props(['name', 'type' => 'text'])
+@props(['name'])
 
 
 <x-form.field>
     <x-form.label name="{{ $name }}"/>
 
     <input
-        class="border border-gray-400 p-2 w-full rounded-xl"
-        type="{{ $type }}"
+        class="border border-gray-200 p-2 w-full rounded-xl"
         name="{{ $name }}"
         id="{{ $name }}"
         value="{{ old($name) }}"
         required
+        {{ $attributes }}
     >
 
     <x-form.error name="{{ $name }}"/>
